e26f3006ce1a08eb4d213e51439626c0392ddf96
MariyaFilippova <maxa.spb6@gmail.com>
2020-05-11 23:33:32 +0300

    Given an undirected graph, return true if and only if it is bipartite.
    Recall that a graph is bipartite if we can split it's set of nodes into two independent subsets A and B such that every edge in the graph has one node in A and another node in B.
    
    The graph is given in the following form: graph[i] is a list of indexes j for which the edge between nodes i and j exists.  Each node is an integer between 0 and graph.length - 1.  There are no self edges or parallel edges: graph[i] does not contain i, and it doesn't contain any element twice.

--- src/IsGraphBipartite.java
+++ src/IsGraphBipartite.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+
 public class IsGraphBipartite {
-//    public boolean isBipartite(int[][] graph) {
-//
-//    }
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (color[i] == 0)
+                dfs(graph, i, color, 1);
+        }
+        return Arrays.stream(color).allMatch(a -> a == 3);
+    }
+
+    int invertColor(int i) {
+        return i == 1 ? 2 : 1;
+    }
+
+    public void dfs(int[][] graph, int v, int[] color, int i) {
+        color[v] = i;
+        for (int e : graph[v]) {
+            if (color[e] == 0) {
+                dfs(graph, e, color, invertColor(i));
+            } else if (color[e] == i) {
+                return;
+            }
+        }
+        color[v] = 3;
+    }
 }

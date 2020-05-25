638aa0909ebe27f1cece582d686d51ea3a0e432e
MariyaFilippova <maxa.spb6@gmail.com>
2020-05-25 10:30:32 +0300

    Given two binary trees, write a function to check if they are the same or not.
    Two binary trees are considered the same if they are structurally identical and the nodes have the same value.

---
+++ src/SameTree.java
@@ -0,0 +1,11 @@
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}

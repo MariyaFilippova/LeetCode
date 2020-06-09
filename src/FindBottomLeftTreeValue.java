a9ce00de725abcde68c25c947db10e60f7ea057c
MariyaFilippova <maxa.spb6@gmail.com>
2020-06-09 19:40:21 +0300

    Given a binary tree, find the leftmost value in the last row of the tree.

--- src/FindBottomLeftTreeValue.java
+++ src/FindBottomLeftTreeValue.java
@@ -5,21 +5,16 @@ public class FindBottomLeftTreeValue {
     public int findBottomLeftValue(TreeNode root) {
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        int c = 0;
         int val = 0;
         while(!q.isEmpty()) {
-            c = q.size();
-            while (c != 0) {
-                TreeNode node = q.poll();
-                val = node.val;
-                if (node.right != null) {
-                    q.add(node.right);
-                }
-                if (node.left != null) {
-                    q.add(node.left);
-                    val = node.left.val;
-                }
-                c--;
+            TreeNode node = q.poll();
+            val = node.val;
+            if (node.right != null) {
+                q.add(node.right);
+            }
+            if (node.left != null) {
+                q.add(node.left);
+                val = node.left.val;
             }
         }
         return val;

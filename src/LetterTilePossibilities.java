9a2fd83cec521a7d0ede772c218edbfb50833a5a
MariyaFilippova <maxa.spb6@gmail.com>
2020-09-24 02:39:35 +0300

    You have n  tiles, where each tile has one letter tiles[i] printed on it.
    Return the number of possible non-empty sequences of letters you can make using the letters printed on those tiles.

---
+++ src/LetterTilePossibilities.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LetterTilePossibilities {
+
+    public int numTilePossibilities(String tiles) {
+        int[] count = new int[26];
+        for (char c : tiles.toCharArray()) count[c - 'A']++;
+        return dfs(count);
+    }
+
+    int dfs(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 0) continue;
+            sum++;
+            arr[i]--;
+            sum += dfs(arr);
+            arr[i]++;
+        }
+        return sum;
+    }
+}

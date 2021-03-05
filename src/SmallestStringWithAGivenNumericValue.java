b3b640abf4ea8feffd1cabb5b64785e6b496ec66
MariyaFilippova <maxa.spb6@gmail.com>
2021-03-05 20:47:26 +0300

    Better solution

--- src/SmallestStringWithAGivenNumericValue.java
+++ src/SmallestStringWithAGivenNumericValue.java
@@ -1,26 +1,14 @@
+import java.util.Arrays;
+
 public class SmallestStringWithAGivenNumericValue {
     public String getSmallestString(int n, int k) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int sum = 0;
-        char current = 'a';
-        while (sum != k) {
-            while ((k - sum - getValue(current) - (n - (stringBuilder.length() + 1)) * getValue('z')) <= 0) {
-                sum += getValue(current);
-                stringBuilder.append(current);
-                if(sum - k == 0) {
-                    break;
-                }
-            }
-            current++;
+        char[] ca = new char[n];
+        Arrays.fill(ca, 'a');
+        k -= n;
+        for (int i = n - 1; i >= 0 && k > 0; --i) {
+            ca[i] += Math.min(25, k);
+            k -= Math.min(k, 25);
         }
-        return stringBuilder.toString();
-    }
-
-    int getValue(char current) {
-        return current - 'a' + 1;
-    }
-
-    public static void main(String[] args) {
-        new SmallestStringWithAGivenNumericValue().getSmallestString(5, 73);
+        return String.valueOf(ca);
     }
 }

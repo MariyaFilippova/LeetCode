ee0d720f6baa4019c360f93bbcb5448d882b0747
MariyaFilippova <maxa.spb6@gmail.com>
2021-03-05 21:25:41 +0300

    On a broken calculator that has a number showing on its display, we can perform two operations:
    Double: Multiply the number on the display by 2, or;
    Decrement: Subtract 1 from the number on the display.
    Initially, the calculator is displaying the number X.
    
    Return the minimum number of operations needed to display the number Y.

---
+++ src/BrokenCalculator.java
@@ -0,0 +1,13 @@
+public class BrokenCalculator {
+    public int brokenCalc(int X, int Y) {
+        if (X > Y) {
+            return X - Y;
+        }
+        if (Y % 2 == 0) {
+            return 1 + brokenCalc(X, Y / 2);
+        }
+        else {
+            return 1 + brokenCalc(X, Y + 1);
+        }
+    }
+}
